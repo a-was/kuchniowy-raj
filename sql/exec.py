3df92311d2e7957f49e21a8b6bf0777833bdc433
Adam Wasilewski <chaper122@gmail.com>
2020-05-10 12:37:13 +0200

    error handling

--- sql/exec.py
+++ sql/exec.py
@@ -8,6 +8,10 @@ sql_files = [f for f in os.listdir('.') if f.endswith('.sql')]
 for sql_file in sql_files:
     with open(sql_file) as f:
         for command in f.read().split(';'):
-            db.execute(command)
+            command = command.strip()
+            try:
+                db.execute(command)
+            except sqlite3.IntegrityError:
+                print('WARNING - This command cannot be executed due to IntegrityError - {}'.format(command))
             db.commit()
 db.close()
