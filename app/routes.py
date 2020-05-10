f4c1154aec8fb0f2110ec981d282458c59877c6d
Adam Wasilewski <chaper122@gmail.com>
2020-05-10 12:37:49 +0200

    user logging basic support

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect, url_for
+
+from . import utils as f
 
 app = Blueprint('main', __name__, template_folder='templates')
 
@@ -9,7 +11,26 @@ def index():
     return render_template('index.html', name=name)
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'GET':
-#         return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if f.authenticate_user(username, password):
+        session['user'] = {
+            'username': username
+        }
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html', err=True)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    rf = request.form
+    f.new_user(rf['nick'], rf['mail'], rf['cooking_level'], rf['sex'], rf['password'])
+    return redirect(url_for('index'))
---
+++ app/utils.py
@@ -0,0 +1,38 @@
+from flask import session, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db import query_db
+
+
+def login_required(f):
+    def decorator(*args, **kwargs):
+        if 'user' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+
+    decorator.__name__ = f.__name__
+    return decorator
+
+
+def authenticate_user(username, password):
+    user_row = query_db("SELECT password FROM users WHERE nick = ?", username)
+    if user_row:
+        return check_password_hash(user_row[0], password)
+    else:
+        return None
+
+
+def new_user(nick, mail, cooking_level, sex, password):
+    from time import time
+    query_db("""
+    INSERT INTO users (nick, mail, password, sex, creation_timestamp, role_id, cooking_level_id) 
+    VALUES (?, ?, ?, ?, ?, 
+        (SELECT role_id FROM roles WHERE name LIKE 'user'), 
+        (SELECT cooking_level_id FROM cooking_levels WHERE name = ? )
+    )
+    """, nick, mail, generate_password_hash(password), sex, int(time()), cooking_level, commit=True)
+
+
+def get_all_recipes():
+    pass
