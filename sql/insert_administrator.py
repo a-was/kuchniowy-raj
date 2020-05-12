ac3fa5ee814f0c0ed176337e0dd0f3cbc339ab68
Adam Wasilewski <chaper122@gmail.com>
2020-05-12 12:13:09 +0200

    insert admin CLI

---
+++ sql/insert_administrator.py
@@ -0,0 +1,29 @@
+from getpass import getpass
+import sqlite3
+
+from werkzeug.security import generate_password_hash
+
+db = sqlite3.connect('../database.sqlite3')
+cursor = db.cursor()
+
+nick_checked = False
+while not nick_checked:
+    nick = input('Nick: ')
+    cursor.execute("SELECT nick FROM users WHERE nick LIKE ?", (nick, ))
+    if cursor.fetchone():
+        print('This nick is taken, please pick another one')
+    else:
+        nick_checked = True
+
+password = getpass('Password: ')
+if password == getpass('Confirm password: '):
+    cursor.execute("""
+        INSERT INTO users (nick, password, role_id) 
+        VALUES (?, ?, 
+        (SELECT role_id FROM roles WHERE name LIKE 'Administrator') ) 
+    """, (nick, generate_password_hash(password)))
+    db.commit()
+    db.close()
+    print('Administrator added')
+else:
+    print('Passwords does not match!')
