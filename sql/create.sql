36219266cf965654dd15f82bea7d31abeb8740fa
Adam Wasilewski <chaper122@gmail.com>
2020-05-12 11:37:30 +0200

    dates, views column
    constraint formatting, dates with default values, last login date added and typo fix

--- sql/create.sql
+++ sql/create.sql
@@ -11,16 +11,16 @@ CREATE TABLE IF NOT EXISTS `roles` (
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `nick` TEXT UNIQUE NOT NULL,
-  `mail` TEXT UNIQUE NOT NULL,
-  `creation_timestamp` INTEGER NOT NULL,
+  `creation_date` DATE NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+  `last_login_date` DATE CHECK ( last_login_date >= creation_date ),
   `role_id` INTEGER NOT NULL,
   `cooking_level_id` INTEGER,
   `sex` TEXT CHECK (sex IN ('F','M')),
   `password` TEXT NOT NULL,
-  CONSTRAINT users_role_id_fk FOREIGN KEY (role_id)
-    REFERENCES roles (role_id) ON DELETE RESTRICT,
-  CONSTRAINT users_cooking_levels_id_fk FOREIGN KEY (cooking_level_id)
-    REFERENCES cooking_levels (cooking_level_id) ON DELETE RESTRICT
+  CONSTRAINT users_role_id_fk
+      FOREIGN KEY (role_id) REFERENCES roles (role_id) ON DELETE RESTRICT,
+  CONSTRAINT users_cooking_levels_id_fk
+      FOREIGN KEY (cooking_level_id) REFERENCES cooking_levels (cooking_level_id) ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS `types_of_food` (
@@ -44,25 +44,26 @@ CREATE TABLE IF NOT EXISTS `recipes` (
   `type_of_food_id` INTEGER,
   `food_category_id` INTEGER,
   `name` TEXT UNIQUE NOT NULL,
-  `creation_timestamp` INTEGER NOT NULL,
+  `creation_date` DATE NOT NULL DEFAULT (DATETIME('now', 'localtime')),
   `description` TEXT NOT NULL,
   `rating` INTEGER CHECK (rating BETWEEN 1 AND 10),
-  `checked` boolean NOT NULL CHECK(checked IN (0,1)),
+  `checked` BOOLEAN NOT NULL DEFAULT 0 CHECK(checked IN (0,1)),
   `time_required` INTEGER NOT NULL,
-  CONSTRAINT recipes_user_id_fk FOREIGN KEY (user_id)
-    REFERENCES users(user_id) ON DELETE SET NULL,
-  CONSTRAINT recipes_type_of_food_id_fk FOREIGN KEY (type_of_food_id)
-    REFERENCES types_of_food (type_of_food_id) ON DELETE RESTRICT,
-  CONSTRAINT recipes_food_category_id_fk FOREIGN KEY (food_category_id)
-    REFERENCES food_categories (food_category_id) ON DELETE RESTRICT  
+  `views` INTEGER DEFAULT 0 CHECK ( views >= 0 ),
+  CONSTRAINT recipes_user_id_fk
+      FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE SET NULL,
+  CONSTRAINT recipes_type_of_food_id_fk
+      FOREIGN KEY (type_of_food_id) REFERENCES types_of_food (type_of_food_id) ON DELETE RESTRICT,
+  CONSTRAINT recipes_food_category_id_fk
+      FOREIGN KEY (food_category_id) REFERENCES food_categories (food_category_id) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS `recepies_ingredients` (
+CREATE TABLE IF NOT EXISTS `recipes_ingredients` (
   `recipe_id` INTEGER NOT NULL,
   `ingredient_id` INTEGER NOT NULL,
   `count` INTEGER NOT NULL,
-  CONSTRAINT recepies_ingredients_recipe_id_fk FOREIGN KEY (recipe_id)
-    REFERENCES recipes (recipe_id) ON DELETE RESTRICT,
-  CONSTRAINT recepies_ingredients_ingredient_id_fk FOREIGN KEY (ingredient_id)
-    REFERENCES ingredients (ingredient_id) ON DELETE RESTRICT
+  CONSTRAINT recipes_ingredients_recipe_id_fk
+      FOREIGN KEY (recipe_id) REFERENCES recipes (recipe_id) ON DELETE RESTRICT,
+  CONSTRAINT recipes_ingredients_ingredient_id_fk
+      FOREIGN KEY (ingredient_id) REFERENCES ingredients (ingredient_id) ON DELETE RESTRICT
 );
