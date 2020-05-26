f3e83e1ebcf50d6d75ef905c8dfba6411f0cda23
EathQ <piotrek.suminski98@gmail.com>
2020-05-26 22:52:16 +0200

    próby-login-register
    To jeszcze nie działa tak jak powinno

--- app/utils/cooking_levels.py
+++ app/utils/cooking_levels.py
@@ -2,4 +2,4 @@ from app.db import query_db, query_db_object
 
 
 def get_cooking_levels():
-    return query_db_object("SELECT id, name, description FROM cooking_levels")
+    return query_db_object("SELECT cooking_level_id, name, description FROM cooking_levels")
