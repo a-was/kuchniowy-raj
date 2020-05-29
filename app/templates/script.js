0b1351a80eed3bed200f2584fb7538d47218ac42
EathQ <piotrek.suminski98@gmail.com>
2020-05-29 21:43:24 +0200

    testy_js
    Próby testowania js na panelu rejestracji

--- app/templates/script.js
+++ app/templates/script.js
@@ -1,35 +1,27 @@
+function scorePassword(pass) {
+  var wynik = 0;
+  var warianty = {
+      cyfry: /\d/.test(pass),
+      male: /[a-z]/.test(pass),
+      duze: /[A-Z]/.test(pass),
+      specjalne: /\W/.test(pass),
+      dlugosc: pass.length > 7
+  };
+  for(var war in warianty)
+    if(warianty[war] == true) wynik += 100 / 5;
 
-    var calculateComplexity = function (password) {
-      var complexity = 0;
-      
-      var regExps = [ 
-        /[a-z]/,
-        /[A-Z]/,
-        /[0-9]/,
-        /.{8}/,
-        /.{16}/,
-    
-      ];
-      
-      regExps.forEach(function (regexp) {
-        if (regexp.test(password)) {
-          complexity++;
-        }
-      });
-      
-      return {
-        value: complexity,
-        max: regExps.length
-      };
-    };
-     
-    var checkPasswordStregth = function (password) {
-      var progress = document.querySelector('#password1'),
-          complexity = calculateComplexity(this.value); 
-      
-      progress.value = complexity.value;
-      progress.max = complexity.max;
-    };
-    
-    var input = document.querySelector('#password1');
-    input.addEventListener('keyup', checkPasswordStregth);
+  var color = '';
+  
+  if(wynik < 50) color ='red';
+  else if(wynik > 50 && wynik < 100) color ='yellow';
+  else if(wynik == 100) color = 'green';
+  $("#strength_score").text(wynik + '%');
+  $("#strength_score").css('background-color', color);
+  return parseInt(wynik);
+}
+
+$(function() {
+  $("#password").on("keypress keyup keydown", function() {
+      scorePassword($(this).val());
+  });
+});
